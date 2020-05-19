202d2dc9a7cf6c3303f0b4eb3568af6b779af2b1
Andre Machida <andmachida@gmail.com>
2020-05-19 00:32:50 -0300

    Implemented auth guards;

--- frontend/src/modules/dashboard/router/index.js
+++ frontend/src/modules/dashboard/router/index.js
@@ -2,6 +2,8 @@ const Dashboard = () => import('../views/Dashboard.vue')
 
 export default [
   {
-    path: '/dashboard', component: Dashboard
+    path: '/dashboard',
+    component: Dashboard,
+    meta: { requiresAuth: true }
   }
 ]
--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -4,6 +4,9 @@ import VueRouter from 'vue-router'
 import authRoutes from '../modules/auth/router'
 import dashboardRoutes from '../modules/dashboard/router'
 
+import AuthService from '../modules/auth/services/auth-service'
+import { AUTH_TOKEN } from '../plugins/apollo'
+
 Vue.use(VueRouter)
 
 const routes = [
@@ -18,6 +21,27 @@ const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
   routes
+})
+
+router.beforeEach(async (to, from, next) => {
+  if (to.matched.some(route => route.meta.requiresAuth)) {
+    const token = window.localStorage.getItem(AUTH_TOKEN)
+    const loginRoute = {
+      path: '/login',
+      query: { redirect: to.fullPath }
+    }
+    if (token) {
+      try {
+        await AuthService.user({ fetchPolicy: 'network-only' })
+        return next()
+      } catch (error) {
+        console.log(error)
+        return next(loginRoute)
+      }
+    }
+    return next(loginRoute)
+  }
+  next()
 })
 
 export default router
