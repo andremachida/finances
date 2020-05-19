ab7ca262d88165d9f75459eed527f47b0273af27
Andre Machida <andmachida@gmail.com>
2020-05-18 23:42:37 -0300

    Created signup;

--- frontend/src/modules/auth/views/Login.vue
+++ frontend/src/modules/auth/views/Login.vue
@@ -146,7 +146,9 @@ export default {
   },
   methods: {
     async submit () {
-      const authData = await AuthService.login(this.user)
+      const authData = this.isLogin ? await AuthService.login(this.user)
+        : await AuthService.signup(this.user)
+
       console.log(authData)
     }
   }
