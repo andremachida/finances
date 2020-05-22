5e84ccb0d30abda78d9bda824f51cfc80d4869b0
Andre Machida <andmachida@gmail.com>
2020-05-21 23:00:04 -0300

    Created account and category service and loading on select;

--- frontend/src/modules/dashboard/modules/finances/views/RecordsAdd.vue
+++ frontend/src/modules/dashboard/modules/finances/views/RecordsAdd.vue
@@ -1,14 +1,99 @@
 <template>
-  <div>
-    teste
-  </div>
+  <v-container text-center>
+    <v-layout row wrap>
+      <v-flex xs12 sm6>
+        <p>Amount</p>
+      </v-flex>
+      <v-flex xs12 sm6>
+        <v-card>
+          <v-card-text>
+            <v-form>
+              <v-select
+                v-model="record.accountId"
+                :items="accounts"
+                item-text="description"
+                item-value="id"
+                name="account"
+                label="Account"
+                prepend-icon="account_balance">
+
+              </v-select>
+              <v-select
+                v-model="record.categoryId"
+                :items="categories"
+                item-text="description"
+                item-value="id"
+                name="category"
+                label="Category"
+                prepend-icon="class">
+
+              </v-select>
+              <v-text-field
+                name="description"
+                label="Description"
+                prepend-icon="description"
+                type="text">
+
+              </v-text-field>
+              <v-text-field
+                name="tags"
+                label="Tags"
+                prepend-icon="label"
+                type="text">
+
+              </v-text-field>
+              <v-text-field
+                name="note"
+                label="Notes"
+                prepend-icon="note"
+                type="text">
+
+              </v-text-field>
+            </v-form>
+          </v-card-text>
+        </v-card>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
+import moment from 'moment'
+import { decimal, minLength, required } from 'vuelidate/lib/validators'
+
+import AccountsService from '../services/accounts-service'
+import CategoriesService from '../services/categories-service'
+
 import { mapActions } from 'vuex'
 
 export default {
   name: 'RecordsAdd',
+  data () {
+    return {
+      accounts: [],
+      categories: [],
+      record: {
+        type: this.$route.query.type.toUpperCase(),
+        amount: 0,
+        date: moment().format('YYYY-MM-DD'),
+        accountId: '',
+        categoryId: '',
+        description: '',
+        tags: '',
+        note: ''
+      }
+    }
+  },
+  validations: {
+    record: {
+      type: { required },
+      amount: { required, decimal, different: value => value !== 0 },
+      date: { required },
+      accountId: { required },
+      categoryId: { required },
+      description: { required, minLength: minLength(6) }
+    }
+  },
   methods: {
     ...mapActions(['setTitle']),
     changeTitle (recordType) {
@@ -26,12 +111,17 @@ export default {
       this.setTitle({ title })
     }
   },
-  beforeRouteUpdate (to, from, next) {
-    this.changeTitle(to.query.type)
+  async beforeRouteUpdate (to, from, next) {
+    const { type } = to.query
+    this.changeTitle(type)
+    this.record.type = type.toUpperCase()
+    this.categories = await CategoriesService.categories({ operation: type })
     next()
   },
-  created () {
+  async created () {
     this.changeTitle(this.$route.query.type)
+    this.accounts = await AccountsService.accounts()
+    this.categories = await CategoriesService.categories({ operation: this.$route.query.type })
   }
 }
 </script>
