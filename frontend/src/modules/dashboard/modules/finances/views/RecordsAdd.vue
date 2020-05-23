5dda9a09764a4a2fbe2b338b5edee0555d4dc595
Andre Machida <andmachida@gmail.com>
2020-05-22 21:37:48 -0300

    Created numeric display; validate form;

--- frontend/src/modules/dashboard/modules/finances/views/RecordsAdd.vue
+++ frontend/src/modules/dashboard/modules/finances/views/RecordsAdd.vue
@@ -1,15 +1,48 @@
 <template>
   <v-container text-center>
     <v-layout row wrap>
-      <v-flex xs12 sm6>
-        <p>Amount</p>
+      <v-flex xs12 sm4 fill-height>
+        <NumericDisplay
+          v-model="$v.record.amount.$model"
+          :color="color" />
       </v-flex>
-      <v-flex xs12 sm6>
+      <v-flex xs12 sm8>
         <v-card>
           <v-card-text>
             <v-form>
+              <v-dialog persistent
+                v-model="showDateDialog"
+                ref="dateDialog"
+                :return-value.sync="record.date"
+                width="290px">
+                <template v-slot:activator="{ on }">
+                  <v-text-field readonly
+                    name="date"
+                    label="Due Date"
+                    prepend-icon="event"
+                    type="text"
+                    v-on="on"
+                    :value="formattedDate" />
+                </template>
+                <v-date-picker scrollable
+                  v-model="dateDialogValue"
+                  :color="color"
+                  locale="pt-BR" >
+                  <v-spacer></v-spacer>
+                  <v-btn text
+                    :color="color"
+                    @click="cancelDateDialog">
+                    Cancel
+                  </v-btn>
+                  <v-btn text
+                    :color="color"
+                    @click="$refs.dateDialog.save(dateDialogValue)">
+                    Ok
+                  </v-btn>
+                </v-date-picker>
+              </v-dialog>
               <v-select
-                v-model="record.accountId"
+                v-model="$v.record.accountId.$model"
                 :items="accounts"
                 item-text="description"
                 item-value="id"
@@ -19,7 +52,7 @@
 
               </v-select>
               <v-select
-                v-model="record.categoryId"
+                v-model="$v.record.categoryId.$model"
                 :items="categories"
                 item-text="description"
                 item-value="id"
@@ -29,6 +62,7 @@
 
               </v-select>
               <v-text-field
+                v-model.trim="$v.record.description.$model"
                 name="description"
                 label="Description"
                 prepend-icon="description"
@@ -36,6 +70,8 @@
 
               </v-text-field>
               <v-text-field
+                v-show="showTagsInput"
+                v-model.trim="record.tags"
                 name="tags"
                 label="Tags"
                 prepend-icon="label"
@@ -43,6 +79,8 @@
 
               </v-text-field>
               <v-text-field
+                v-show="showNoteInput"
+                v-model.trim="record.note"
                 name="note"
                 label="Notes"
                 prepend-icon="note"
@@ -50,8 +88,42 @@
 
               </v-text-field>
             </v-form>
+            <v-tooltip left>
+              <template v-slot:activator="{ on }">
+                <v-btn icon small
+                  class="mr-3"
+                  v-on="on"
+                  @click="showTagsInput = !showTagsInput">
+                  <v-icon :color="color">label</v-icon>
+                </v-btn>
+              </template>
+              <span>Add Tags</span>
+            </v-tooltip>
+            <v-tooltip right>
+              <template v-slot:activator="{ on }">
+                <v-btn icon small
+                  v-on="on"
+                  @click="showNoteInput = !showNoteInput">
+                  <v-icon :color="color">note</v-icon>
+                </v-btn>
+              </template>
+              <span>Notes</span>
+            </v-tooltip>
           </v-card-text>
         </v-card>
+        <v-btn large fab
+          color="secondary"
+          class="mt-4"
+          @click="$router.back()">
+          <v-icon>close</v-icon>
+        </v-btn>
+        <v-btn large fab
+          :color="color"
+          class="mt-4"
+          :disabled="$v.$invalid"
+          @click="submit">
+          <v-icon>check</v-icon>
+        </v-btn>
       </v-flex>
     </v-layout>
   </v-container>
@@ -64,10 +136,30 @@ import { decimal, minLength, required } from 'vuelidate/lib/validators'
 import AccountsService from '../services/accounts-service'
 import CategoriesService from '../services/categories-service'
 
+import NumericDisplay from '../components/NumericDisplay.vue'
+
 import { mapActions } from 'vuex'
 
 export default {
   name: 'RecordsAdd',
+  components: {
+    NumericDisplay
+  },
+  computed: {
+    color () {
+      switch (this.record.type) {
+        case 'CREDIT':
+          return 'primary'
+        case 'DEBIT':
+          return 'error'
+        default:
+          return 'primary'
+      }
+    },
+    formattedDate () {
+      return moment(this.record.date).format('DD/MM/YYYY')
+    }
+  },
   data () {
     return {
       accounts: [],
@@ -81,7 +173,11 @@ export default {
         description: '',
         tags: '',
         note: ''
-      }
+      },
+      showNoteInput: false,
+      showTagsInput: false,
+      showDateDialog: false,
+      dateDialogValue: moment().format('YYYY-MM-DD')
     }
   },
   validations: {
@@ -109,12 +205,20 @@ export default {
           title = 'New Release'
       }
       this.setTitle({ title })
+    },
+    cancelDateDialog () {
+      this.showDateDialog = false
+      this.dateDialogValue = this.record.date
+    },
+    submit () {
+      console.log(this.record)
     }
   },
   async beforeRouteUpdate (to, from, next) {
     const { type } = to.query
     this.changeTitle(type)
     this.record.type = type.toUpperCase()
+    this.record.categoryId = ''
     this.categories = await CategoriesService.categories({ operation: type })
     next()
   },
